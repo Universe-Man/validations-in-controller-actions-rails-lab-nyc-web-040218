2831bf43653136e9acb5683c2a58c64ffe4a2766
github username <ianjpollack@gmail.com>
2018-05-01 10:38:02 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,14 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    @temp = Post.new(post_params)
+    @post = Post.find(params[:id])
+    if @temp.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
